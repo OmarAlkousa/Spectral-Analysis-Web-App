005e4e476e5791d1f7ece0136ada7d793916ce30
Omar Alkousa <omar.ok1998@gmail.com>
2023-07-05 00:11:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ if uploaded_file is not None:
     # Caching the data for faster implementation
     @st.cache_data
     def load_signal():
-        sig = np.loadtxt(uploaded_file.name)
+        sig = np.loadtxt(uploaded_file.name, delimiter=',')
         return sig
 
     # Load the Data
